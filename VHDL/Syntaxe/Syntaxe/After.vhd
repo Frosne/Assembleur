2e19d87677b1bc4c4afaf30958373bbf382845c1
Anilam <malina.wein@gmail.com>
2017-08-01 14:59:38 +0200

    After

---
+++ VHDL/Syntaxe/Syntaxe/After.vhd
@@ -0,0 +1,15 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+entity nor2 is
+	port (a,b: in std_logic;
+	z: out std_logic);
+end nor2;
+
+ARCHITECTURE behaviour of nor2 is
+begin 
+	dly0: process(a,b)
+		begin
+		end process dly0;
+end behaviour; 		
+		
