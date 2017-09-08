0550f1301ef7f7a3948b8c754fd0c5afa320b7a5
Anilam <malina.wein@gmail.com>
2017-09-08 14:21:31 +0200

    dev

---
+++ VHDL/LCD1602/LCD1602/lcd1602Driver.vhd
@@ -0,0 +1,128 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    23:29:21 09/07/2017 
+-- Design Name: 
+-- Module Name:    lcd1602Driver - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity lcd1602Driver is
+	port (
+	clk : in std_logic;
+	
+	reset: in std_logic;
+	lcd_rs: out std_logic;
+	lcd_rw: out std_logic;
+	lcd_en: out std_logic;
+
+	lcd_n: out std_logic;
+	lcd_p: out std_logic;
+
+	vss: out std_logic;
+	vdd: out std_logic;
+	vo: out std_logic;
+
+	lcd_data_out: out std_logic_vector (7 downto 0)
+	);
+end lcd1602Driver;
+
+architecture Behavioral of lcd1602Driver is
+
+    component lcd1602Controller is
+     	port (
+		 	clk : IN  std_logic;
+		    clk_out : INOUT  std_logic;
+		    reset : IN  std_logic;
+		    lcd_rs : OUT  std_logic;
+		    lcd_rw : OUT  std_logic;
+		    lcd_en : OUT  std_logic;
+		    lcd_n : OUT  std_logic;
+		    lcd_p : OUT  std_logic;
+		    vss : OUT  std_logic;
+		    vdd : OUT  std_logic;
+		    vo : OUT  std_logic;
+		    position : IN  std_logic_vector(7 downto 0);
+		    lcd_data_in : IN  std_logic_vector(7 downto 0);
+		    lcd_data_out : OUT  std_logic_vector(7 downto 0);
+		    request : INout  std_logic;
+			busy_out : out std_logic	
+     	);
+    end component; 
+    
+    --Inputs
+    signal position : std_logic_vector(7 downto 0) := (others => '0');
+    signal lcd_data_in : std_logic_vector(7 downto 0) := (others => '0');
+    signal request : std_logic := '0';
+
+ 	--Outputs
+    signal clk_out : std_logic;
+	 signal busy_out: std_logic;
+	 
+	 signal clk_lcd : std_logic := '0' ;
+
+	--signal request_e : std_logic := '0';
+
+begin
+
+	process (clk)
+		variable clk_temp: integer range 0 to 199999;
+	begin	
+		if rising_edge(CLK) then 
+			if (clk_temp < 10) then 
+				clk_temp := clk_temp +1;
+			else 
+				clk_temp := 0;
+				clk_lcd <= not clk_lcd;
+			end if;
+		end if;	
+	end process;
+	
+	request <= '1' when (busy_out = '0') else 'Z';
+	position <= "10000010";
+	lcd_data_in <= x"42";
+
+	lcd: lcd1602Controller port map (
+          clk => clk,
+          clk_out => clk_out,
+          reset => reset,
+          lcd_rs => lcd_rs,
+          lcd_rw => lcd_rw,
+          lcd_en => lcd_en,
+          lcd_n => lcd_n,
+          lcd_p => lcd_p,
+          vss => vss,
+          vdd => vdd,
+          vo => vo,
+          position => position,
+          lcd_data_in => lcd_data_in,
+          lcd_data_out => lcd_data_out,
+          request => request,
+	    busy_out => busy_out
+		);
+end Behavioral;
+
--- VHDL/LCD1602/lcdController.vhd
+++ VHDL/LCD1602/lcdController.vhd
@@ -7,6 +7,8 @@ entity lcd1602Controller is
 	port (
 
 		clk : in std_logic;
+		clk_out: inout std_logic;
+		
 		reset: in std_logic;
 		lcd_rs: out std_logic;
 		lcd_rw: out std_logic;
@@ -23,7 +25,8 @@ entity lcd1602Controller is
 
 		lcd_data_in: in std_logic_vector(7 downto 0);
 		lcd_data_out: out std_logic_vector (7 downto 0);
-		request : inout std_logic
+		request : inout std_logic;
+		busy_out : out std_logic
 		
 	);
 end entity lcd1602Controller;
@@ -33,16 +36,14 @@ architecture beh of lcd1602Controller is
 		(main, clearDisplay, function_set, display_on_off,
 			entry_mode_set, write_cgram, set_ddram, write_data);
 	signal currentState : state;
-	signal clk1: std_logic;
-	signal clk_out: std_logic;
+	--signal clk1: std_logic;
+	--signal clk_out: std_logic;
 	signal lcd_clk: std_logic;
-
-	signal inited : std_logic;
-	signal busy : std_logic;
+	--signal temp: std_logic := '0';
+	signal inited : std_logic := '0';
+	signal busy : std_logic := '0';
 	
 	begin
-		inited <= '0';
-		busy <= '0';
 		lcd_en <= clk_out;
 		lcd_rw <= '0';
 		lcd_n <= '0';
@@ -51,43 +52,48 @@ architecture beh of lcd1602Controller is
 		vdd<= '1';
 		vo <= '1';
 
-	process	(clk)
-		variable n1: integer range 0 to 19999999;
-		begin
-			if rising_edge(clk) then 
-				if n1 < 19999999 then 
-					n1:= n1 + 1;
-				else 
-					n1:=0;
-					clk_out <= not clk_out; 
-				end if;
-			end if;		
-	end process;
-
-	lcd_clk <= clk_out;
-
-	process (clk_out)
-		variable n2: integer range 0 to 499;
-		begin 
-			if rising_edge (clk_out) then
-				if n2 < 499 then
-					n2:= n2 + 1;
-				else 
-					n2:= 0;	
-					clk1 <=  not clk1;
-				end if;
-			end if;		
-	end process;		
+		
+		process	(clk)
+			variable n1: integer range 0 to 2;
+			variable temp : std_logic := '0';
+			begin
+				if rising_edge(clk) then 
+					if n1 < 2 then 
+						n1:= n1 + 1;
+					else 
+						n1:=0;
+						temp := not temp;
+						clk_out <= temp;
+						lcd_clk <= temp;
+					end if;
+				end if;		
+		end process;
+		
+		--lcd_clk <= clk_out;
+	
+	request <= '0' when busy = '1' else 'Z';
+	busy_out <= busy;
 
 	process (request, currentState, reset, lcd_data_in, position, lcd_clk, inited, busy)
 		begin 
 			if reset = '0' then 
 				currentState <= main;
 				lcd_rs <= '0';
-			elsif request = '1' and rising_edge(LCD_Clk) then
+			elsif busy = '0' and request = '1' then
 				lcd_rs <= '0';
+				if busy = '1' then 
+					currentState<= currentState;
+				elsif inited = '0' then
+				   busy <= '1';
+					currentState <= clearDisplay;
+				else
+					busy <= '1';
+					currentState <= write_cgram;	
+				end if;
+			elsif busy = '1' and rising_edge(LCD_Clk) then
 				case currentState is 
 					when main => 
+						lcd_rs <= '0';
 						if busy = '1' then 
 							currentState<= currentState;
 						elsif inited = '0'
@@ -95,11 +101,13 @@ architecture beh of lcd1602Controller is
 						else 
 							currentState <= write_cgram;	
 						end if;	
-					when clearDisplay => 
+					when clearDisplay =>
+						lcd_rs <= '0'; 
 						busy <= '1';
 						lcd_data_out <= "00000001";
 						currentState <= function_set;
 					when function_set => -- function set
+						lcd_rs <= '0';
 						-- 001 DL N F * *
 						-- DL - data length 0 = 4-bit interface, 1 = 8-bit interface;
 						-- N - number of display line N - 0 = 1/8 or 1/11 duty (1 line), 1 = 1/16 duty (2 lines);
@@ -108,6 +116,7 @@ architecture beh of lcd1602Controller is
 						lcd_data_out <= "00111000";
 						currentState <= display_on_off;
 					when display_on_off => 
+						lcd_rs <= '0';
 						lcd_data_out <= "00001100";
 						-- 00001 D C B 
 						-- D - on off all display
@@ -115,6 +124,7 @@ architecture beh of lcd1602Controller is
 						-- B - blinking on off
 						currentState <= entry_mode_set;
 					when entry_mode_set => 
+						lcd_rs <= '0';
 						lcd_data_out <= "00000110";
 						-- 00000 1 i/D S
 						-- I/D - 0 = decrement cursor position,
@@ -130,15 +140,17 @@ architecture beh of lcd1602Controller is
 						currentState <= set_ddram;
 						-- &mcar[data_in]
 					when set_ddram =>
+						lcd_rs <= '0';
  						lcd_data_out <= position;
 						currentState <= write_data;		
 						busy<= '1';			
-					when write_data => 
+					when write_data =>
+						lcd_rs <= '0'; 
 						lcd_data_out <= "00000000";
 						currentState <= write_cgram;
 						busy <= '0';
-						request <= '0';
-					when others => null;		
+						--request <= '0' when wri else 'Z';
+					when others => null;	
 				end case;
 			end if;
 		end process;			
