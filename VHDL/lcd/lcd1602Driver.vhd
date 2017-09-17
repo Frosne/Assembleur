505ad4f1157a4aae721bb493a47b5846e0d19578
Kyo Dralliam <chocobodralliam@gmail.com>
2017-09-17 17:27:33 +0200

    adapt lcd driver to a simple test

--- VHDL/lcd/lcd1602Driver.vhd
+++ VHDL/lcd/lcd1602Driver.vhd
@@ -7,7 +7,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity lcd1602Driver is
     port (
     clk : in std_logic;
-    
+
     reset: in std_logic;
     lcd_rs: out std_logic;
     lcd_rw: out std_logic;
@@ -20,7 +20,9 @@ entity lcd1602Driver is
     vdd: out std_logic;
     vo: out std_logic;
 
-    lcd_data_out: out std_logic_vector (7 downto 0)
+    lcd_data_out: out std_logic_vector (7 downto 0);
+    led : out std_logic_vector(3 downto 0)
+
     );
 end lcd1602Driver;
 
@@ -42,10 +44,11 @@ architecture Behavioral of lcd1602Driver is
             lcd_data_in : IN  std_logic_vector(7 downto 0);
             lcd_data_out : OUT  std_logic_vector(7 downto 0);
             request : in  std_logic;
-            busy_out : out std_logic
+            busy_out : out std_logic;
+            led : out std_logic_vector(3 downto 0)
         );
-    end component; 
-    
+    end component;
+
     --Inputs
     signal position : std_logic_vector(6 downto 0) := (others => '0');
     signal lcd_data_in : std_logic_vector(7 downto 0) := (others => '0');
@@ -55,48 +58,62 @@ architecture Behavioral of lcd1602Driver is
 
     signal displayed_char : std_logic_vector(7 downto 0) := x"42";
     signal busy_out : std_logic;
-    
+
 begin
     position <= "0000010";
-    
-    process (clk)
-        variable counter: integer range 0 to 4999999;
-    begin
-        if rising_edge(clk) and counter < 4999999 then
-            counter := counter + 1;
-            --request <='0';
-        end if;
-        if counter < 10000 then
-            request <= '0';
-        elsif counter < 12000 then
-            request <= '1';
-        elsif counter < 4999997 then
-            request <= '0';
-        else
-            request <= '1';
-        end if;
-    end process;
-
-    process (clk)
-        variable char_acc : integer range 0 to 5000000;
-        variable cc : std_logic := '0';
-    begin
-        if rising_edge(clk) then
-            if (char_acc < 5000000) then
-                char_acc := char_acc + 1;
-            else
-                char_acc := 0;
-                cc := not cc;
-                if cc = '1' then
-                    displayed_char <= x"45";
-                else
-                    displayed_char <= x"42";
-                end if;
-            end if;
-        end if;
-    end process;
-
     lcd_data_in <= displayed_char;
+
+    process (clk)
+      variable acc : integer range 0 to 50000000 := 0;
+    begin
+      if rising_edge(clk) then
+        if acc = 50000000 then
+          request <= '1';
+        else
+          acc := acc + 1;
+        end if;
+      end if;
+    end process;
+
+
+    -- process (clk)
+    --     variable counter: integer range 0 to 4999999;
+    -- begin
+    --     if rising_edge(clk) and counter < 4999999 then
+    --         counter := counter + 1;
+    --         --request <='0';
+    --     end if;
+    --     if counter < 10000 then
+    --         request <= '0';
+    --     elsif counter < 12000 then
+    --         request <= '1';
+    --     elsif counter < 4999997 then
+    --         request <= '0';
+    --     else
+    --         request <= '1';
+    --     end if;
+    -- end process;
+
+    -- process (clk)
+    --     variable char_acc : integer range 0 to 5000000;
+    --     variable cc : std_logic := '0';
+    -- begin
+    --     if rising_edge(clk) then
+    --         if (char_acc < 5000000) then
+    --             char_acc := char_acc + 1;
+    --         else
+    --             char_acc := 0;
+    --             cc := not cc;
+    --             if cc = '1' then
+    --                 displayed_char <= x"45";
+    --             else
+    --                 displayed_char <= x"42";
+    --             end if;
+    --         end if;
+    --     end if;
+    -- end process;
+
+    -- lcd_data_in <= displayed_char;
 
     lcd: lcd1602Controller port map (
           clk => clk,
@@ -113,7 +130,8 @@ begin
           lcd_data_in => lcd_data_in,
           lcd_data_out => lcd_data_out,
           request => request,
-          busy_out => busy_out
+          busy_out => busy_out,
+          led => led
         );
 end Behavioral;
 
--- VHDL/lcd/lcdController.vhd
+++ VHDL/lcd/lcdController.vhd
@@ -7,7 +7,7 @@ entity lcd1602Controller is
     port (
 
         clk : in std_logic;
-        
+
         reset: in std_logic;
         lcd_rs: out std_logic;
         lcd_rw: out std_logic;
@@ -25,82 +25,90 @@ entity lcd1602Controller is
         lcd_data_in: in std_logic_vector(7 downto 0);
         lcd_data_out: out std_logic_vector (7 downto 0);
         request : in std_logic;
-        busy_out: out std_logic
+        busy_out: out std_logic;
+        led : out std_logic_vector(3 downto 0)
     );
 end entity lcd1602Controller;
 
 
 architecture beh of lcd1602Controller is
-    type state is 
+    type state is
         (waiting, clearDisplay, function_set, display_on_off,
             entry_mode_set, set_position, write_char, display);
     signal currentState : state;
 	 signal rising_edge_request : std_logic;
-	 signal busy: std_logic:='0'; 
+	 signal busy: std_logic:='0';
 	 signal inited: std_logic := '0';
 	 signal clk_lcd: std_logic;
-	
-    
+
+
 	 begin
+        lcd_en <= clk_lcd;
         lcd_rw <= '0';
         lcd_n <= '0';
         lcd_p <= '1';
         vss<= '0';
         vdd<= '1';
         vo <= '1';
-		  
+
+        led(0) <= busy;
+        led(1) <= inited;
+
 	process(clk)
 		variable counter:integer range 0 to 19999;
-		begin 
+		begin
 		if rising_edge(clk) then
 			if counter<19999 then
 				counter:=counter+1;
-			else 
+			else
 				counter:=0;
 				clk_lcd<=not clk_lcd;
 			end if;
 		end if;
-	end process;	  
-		  
-		  
-	process(clk, request)
-	variable request_status : std_logic := '0';
-	variable request_status_before: std_logic := '0';
-		begin
-			request_status_before := request_status;
-			request_status := request;
-			if (request_status_before = '0' and request_status ='1') then
-				rising_edge_request <= '1';
-			else 
-				rising_edge_request <= '0';
-			end if;
-			
 	end process;
-	
+
+
+  process(clk_lcd, request)
+    variable request_status : std_logic := '0';
+    variable request_status_before: std_logic := '0';
+  begin
+    -- Do we need to explicitly use a rising_edge(clk)
+    if rising_edge(clk_lcd) then
+      request_status_before := request_status;
+      request_status := request;
+      if (request_status_before = '0' and request_status ='1') then
+        rising_edge_request <= '1';
+      else
+        rising_edge_request <= '0';
+      end if;
+    end if;
+
+  end process;
+
 	process (clk_lcd, reset, request, currentState)
 		begin
-			if reset = '1' then --?
+			if reset = '0' then --?
 				inited <='0';
 				currentState <= waiting;
 				lcd_rs <='0';
-				
+
 			elsif rising_edge_request = '1' and busy = '0' then
 				busy <= '1';
-				if inited = '0' then 
+				if inited = '0' then
 					currentState <= clearDisplay;
 				else
 					currentState <= set_position;
 				end if;
-				
-			elsif rising_edge(clk_lcd) then				
+
+			elsif rising_edge(clk_lcd) then
 				currentState <= currentState ;
 				lcd_rs <='0';
-				
+
 			case currentState is
-					when clearDisplay => 			
+					when clearDisplay =>
 						lcd_data_out <= "00000001";
 						currentState <= function_set;
-					when function_set => 
+					when function_set =>
 						lcd_data_out <= "00111000";
 						currentState <= display_on_off;
 					when display_on_off =>
@@ -111,23 +119,23 @@ architecture beh of lcd1602Controller is
 						currentState <= set_position;
 						inited <= '1';
 					when set_position =>
-						lcd_data_out <= '1' & position;
+						lcd_data_out <= "10000100"; --'1' & position;
 						currentState <= write_char;
 					when write_char =>
 						lcd_rs <= '1';
-						lcd_data_out <= lcd_data_in;
+						lcd_data_out <= x"42";-- lcd_data_in;
 						currentState <= display;
-					when display => 
+					when display =>
 						lcd_data_out <= "00000000";
-						currentState <= waiting;
+						currentState <= clearDisplay;
 						busy <= '0';
 					when others =>
 						currentState <= waiting;
 				end case;
-			end if;	
-			
-	end process;	
-           
+			end if;
+
+	end process;
+
 end architecture beh;
 
 
